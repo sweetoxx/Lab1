5ec02416d44000be1789835b61a9975cb7b91f56
sweetoxx <90527852+sweetoxx@users.noreply.github.com>
2022-12-25 20:36:54 +0300

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+if (!$_SESSION['user']) {
+    header('Location: /');
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Raddish</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+    <!-- Профиль -->
+
+    <form>
+        <img src="<?= $_SESSION['user']['avatar'] ?>" width="400" alt="">
+        <h2 style="margin: 20px 0;"><?= $_SESSION['user']['full_name'] ?></h2>
+        <a href="#"><?= $_SESSION['user']['email'] ?></a>
+        <a href="vendor/logout.php" class="logout">Выход</a>
+    </form>
+
+</body>
+</html>
